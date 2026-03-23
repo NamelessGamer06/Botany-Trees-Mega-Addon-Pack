890079b0a9cbb3d66901b8cd363d867af461c684
NamelessGamer06 <josiah.hoffman4106@gmail.com>
2026-03-23 13:26:27 -0400

    Init Upload

---
+++ src/main/java/com/nameless/botanytreesmegaaddonpack/BotanyTreesMegaAddonPack.java
@@ -0,0 +1,37 @@
+package com.nameless.botanytreesmegaaddonpack;
+
+import org.slf4j.Logger;
+
+import com.mojang.logging.LogUtils;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.server.ServerStartingEvent;
+
+
+@Mod(BotanyTreesMegaAddonPack.MODID)
+public class BotanyTreesMegaAddonPack {
+    public static final String MODID = "botanytreesmegaaddonpack";
+    public static final Logger LOGGER = LogUtils.getLogger();
+
+    public BotanyTreesMegaAddonPack(IEventBus modEventBus, ModContainer modContainer) {
+        modEventBus.addListener(this::commonSetup);
+
+        NeoForge.EVENT_BUS.register(this);
+    }
+
+    private void commonSetup(FMLCommonSetupEvent event) {
+        LOGGER.info("HELLO FROM COMMON SETUP");
+    }
+
+    // You can use SubscribeEvent and let the Event Bus discover methods to call
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event) {
+        // Do something when the server starts
+        LOGGER.info("HELLO from server starting");
+    }
+}
